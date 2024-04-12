0c6be5ef6e5ae4f2753f5317672953ec1119a4f3
Rebeca <rbenedetti@itsynch.com>
2024-04-12 13:01:19 -0300

    feat: create post form component

---
+++ src/app/post/components/post-form/post-form.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { filter, map } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { Post } from '../../models/post';
+import { PostsService } from '../../posts.service';
+
+@Component({
+  selector: 'app-post-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterLink,
+  ],
+  templateUrl: './post-form.component.html',
+})
+export class PostFormComponent {
+  public postForm = this.formBuilder.group({
+    user: this.formBuilder.control('', [Validators.required]),
+    content: this.formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(50),
+    ]),
+  });
+
+  public userErrors$ = this.postForm.statusChanges.pipe(
+    map((x) => this.postForm.get('user') as AbstractControl),
+    filter((x) => x != null),
+    map((abstractControl) => {
+      const errors = [];
+      if (abstractControl.getError('required')) {
+        errors.push('The user is required');
+      }
+      return errors;
+    })
+  );
+
+  public contentErrors$ = this.postForm.statusChanges.pipe(
+    map((x) => this.postForm.get('content') as AbstractControl),
+    filter((x) => x != null),
+    map((abstractControl) => {
+      const errors = [];
+      if (abstractControl.getError('required')) {
+        errors.push('The content is required');
+      }
+      if (abstractControl.getError('minlength')) {
+        errors.push('The post min legth is 50');
+      }
+      return errors;
+    })
+  );
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private postsService: PostsService,
+    private router: Router
+  ) {}
+
+  submit() {
+    const user = this.postForm.value.user;
+    const content = this.postForm.value.content;
+    const postData: Post = {
+      user: user ?? '',
+      published: new Date(),
+      content: content ?? '',
+    };
+    this.postsService.addPost(postData);
+    this.router.navigate(['posts/list']);
+  }
+}
