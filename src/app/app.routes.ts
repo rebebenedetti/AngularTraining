cac13213f9157a245bd152541879a542b1dc35a2
Rebeca <rbenedetti@itsynch.com>
2024-04-12 13:04:22 -0300

    feat: add routing lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'posts',
+    loadChildren: () => import('./post/post.module').then((x) => x.PostModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'posts',
+  },
+];
--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -5,19 +5,44 @@ import { PostListComponent } from './components/post-list/post-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PostsService } from './posts.service';
 import { POST_SERVICE_URL } from './injection-token/post-service-url';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+
+export const routes: Routes = [
+  {
+    path: 'list',
+    component: PostListComponent,
+  },
+  {
+    path: 'create',
+    component: PostFormComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'list',
+    pathMatch: 'full',
+  },
+];
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, PostListComponent,
-    PostComponent, HttpClientModule
+    CommonModule,
+    PostListComponent,
+    PostComponent,
+    PostFormComponent,
+    HttpClientModule,
+    RouterModule.forChild(routes),
+    MatButtonModule,
   ],
-  providers:[{
-    provide: POST_SERVICE_URL,
-    useValue: "https://asmisalan.github.io/feedgram/posts.json"
-  },
-    PostsService
+  providers: [
+    {
+      provide: POST_SERVICE_URL,
+      useValue: 'https://asmisalan.github.io/feedgram/posts.json',
+    },
+    PostsService,
   ],
-  exports: [PostListComponent
-  ]
+  exports: [],
 })
-export class PostModule { }
+export class PostModule {}
