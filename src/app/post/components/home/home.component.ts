56a3e1c96c0b6da51e3ec9ffc3c8b8a9df440715
Rebeca <rbenedetti@itsynch.com>
2024-04-12 16:57:51 -0300

    feat: add home component

---
+++ src/app/post/components/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MatButton, RouterLink],
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {}
