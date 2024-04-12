784ce369ce683bd2b2b4849b42e8bf3c000ce847
Rebeca <rbenedetti@itsynch.com>
2024-04-12 16:58:56 -0300

    feat: add routing lazy loading

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -1,41 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PostComponent } from './components/post/post.component';
-import { PostListComponent } from './components/post-list/post-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PostsService } from './posts.service';
 import { POST_SERVICE_URL } from './injection-token/post-service-url';
-import { PostFormComponent } from './components/post-form/post-form.component';
 import { RouterModule, Routes } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
 
 export const routes: Routes = [
   {
     path: 'list',
-    component: PostListComponent,
+    loadComponent: () =>
+      import('./components/post-list/post-list.component').then(
+        (x) => x.PostListComponent
+      ),
   },
   {
     path: 'create',
-    component: PostFormComponent,
+    loadComponent: () =>
+      import('./components/post-form/post-form.component').then(
+        (x) => x.PostFormComponent
+      ),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./components/home/home.component').then((x) => x.HomeComponent),
   },
   {
     path: '',
-    redirectTo: 'list',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    PostListComponent,
-    PostComponent,
-    PostFormComponent,
-    HttpClientModule,
-    RouterModule.forChild(routes),
-    MatButtonModule,
-  ],
+  imports: [CommonModule, HttpClientModule, RouterModule.forChild(routes)],
   providers: [
     {
       provide: POST_SERVICE_URL,
