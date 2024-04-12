876a59be725b73340ccb9c23aceb02c6931a395a
Rebeca <rbenedetti@itsynch.com>
2024-04-12 17:00:42 -0300

    feat: add button to redirect to create a post

--- src/app/post/components/post-list/post-list.component.ts
+++ src/app/post/components/post-list/post-list.component.ts
@@ -3,21 +3,26 @@ import { PostComponent } from '../post/post.component';
 import { Post } from '../../models/post';
 import { CommonModule } from '@angular/common';
 import { PostsService } from '../../posts.service';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-post-list',
   standalone: true,
   templateUrl: './post-list.component.html',
-  styleUrl: './post-list.component.scss',
-  imports: [CommonModule, PostComponent],
+  imports: [CommonModule, PostComponent, MatButtonModule],
 })
 
 //SmartComponent
 export class PostListComponent {
   public posts!: Post[];
 
-  constructor(private postService: PostsService) {}
+  constructor(private postService: PostsService, private router: Router) {}
 
   ngOnInit() {
     this.posts = this.postService.allPosts;
+  }
+
+  AddPost() {
+    this.router.navigate(['posts/create']);
   }
 }
