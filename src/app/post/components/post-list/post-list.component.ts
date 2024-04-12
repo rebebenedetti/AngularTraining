97bfc82aa5fc0715244fcaa8397a49508fce8c64
Rebeca <rbenedetti@itsynch.com>
2024-04-12 13:02:00 -0300

    feat: store retrieved posts in local property and add 'addPost' method

--- src/app/post/components/post-list/post-list.component.ts
+++ src/app/post/components/post-list/post-list.component.ts
@@ -1,27 +1,23 @@
 import { Component } from '@angular/core';
-import { PostComponent } from "../post/post.component";
+import { PostComponent } from '../post/post.component';
 import { Post } from '../../models/post';
 import { CommonModule } from '@angular/common';
 import { PostsService } from '../../posts.service';
-import { Observable } from 'rxjs';
-
 @Component({
   selector: 'app-post-list',
   standalone: true,
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss',
-  imports: [CommonModule, PostComponent]
+  imports: [CommonModule, PostComponent],
 })
 
 //SmartComponent
 export class PostListComponent {
+  public posts!: Post[];
 
-  public posts$!: Observable<Post[]>;
-
-  constructor(private postService: PostsService) { }
+  constructor(private postService: PostsService) {}
 
   ngOnInit() {
-    this.posts$ = this.postService.getAllPost();
+    this.posts = this.postService.allPosts;
   }
-
 }
--- src/app/post/posts.service.ts
+++ src/app/post/posts.service.ts
@@ -2,13 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Post } from './models/post';
 import { POST_SERVICE_URL } from './injection-token/post-service-url';
+import { map, take } from 'rxjs';
 
 @Injectable()
 export class PostsService {
+  public allPosts: Post[] = [];
 
-  constructor(@Inject(POST_SERVICE_URL) public serviceURL: string, private httpClient: HttpClient) { }
+  constructor(
+    @Inject(POST_SERVICE_URL) public serviceURL: string,
+    private httpClient: HttpClient
+  ) {
+    this.httpClient
+      .get<Post[]>(this.serviceURL)
+      .pipe(
+        take(1),
+        map((posts) => posts.map((post) => this.allPosts.push(post)))
+      )
+      .subscribe();
+  }
 
-  getAllPost() {
-    return this.httpClient.get<Post[]>(this.serviceURL)
+  addPost(post: Post) {
+    this.allPosts.push(post);
   }
 }
